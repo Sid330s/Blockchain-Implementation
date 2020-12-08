27765101cb2e8c72a0441f58093d486a8e273258
Siddharth Kale <52232241+Sid330s@users.noreply.github.com>
2020-12-08 08:57:56 +0530

    No change

---
+++ src/main/java/com/example/Blockchain/NodeObject.java
@@ -0,0 +1,9 @@
+package com.example.Blockchain;
+
+public class NodeObject {
+    String node_address;
+
+    public NodeObject(String node_address) {
+        this.node_address = node_address;
+    }
+}
